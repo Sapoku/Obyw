5eac9cc53396c2fef9648b5bef1b452f51306d7a
Sapoku <starik1998s@mail.ru>
2018-04-23 16:49:02 +0300

    Chetvertii

---
+++ PR1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    class Obyw
+    {
+        public string proizv { get; set; }
+        public float razm { get; set; }
+        public double sena { get; set; }
+    }
+}
