3f7e3fcb640b295db2a9026386ac98d8731ca79e
Sapoku <starik1998s@mail.ru>
2018-04-23 18:48:43 +0300

    TEPER TOCHNO LAST

---
+++ PR1/Footwear.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR1
+{/// <summary>
+/// Описываем класс обувь
+/// </summary>
+    class Footwear
+    {
+        public string Manufacturer { get; set; }
+        public float Size { get; set; }
+        public double Price { get; set; }
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public Footwear()
+        {
+
+        }
+        /// <summary>
+        /// Ввод объектов класса с клавиатуры
+        /// </summary>
+        public void Input()
+        {
+            Manufacturer = Console.ReadLine();
+            Size = float.Parse(Console.ReadLine());
+            Price = Double.Parse(Console.ReadLine());
+        }
+    }
+}
--- PR1/Program.cs
+++ PR1/Program.cs
@@ -10,31 +10,14 @@ namespace PR1
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Obyw obyw = new Obyw();
-            obyw.Input();
-=======
+            Footwear Footwear = new Footwear();
+            Footwear.Input();
+
             Console.WriteLine("HALO WARLD");
             Console.ReadKey();
->>>>>>> c27bc5bf708a863cc1b12f8401c9df77de91a001
+
         }
     }
 
 
-<<<<<<< HEAD
-    
-=======
-    class Obyw
-    {
-        public string proizv { get; set; }
-        public float razm { get; set; }
-        public double sena { get; set; }
-
-        public Obyw()
-        {
-            
-        }
-    }
-   
->>>>>>> c27bc5bf708a863cc1b12f8401c9df77de91a001
 }
